d751f7fc99f39abb1ec85f3bdff61d2aa86a4cec
loadfms@gmail.com <loadfms@gmail.com>
2018-04-09 20:40:03 -0300

    fix - route

--- devices/main.go
+++ devices/main.go
@@ -14,11 +14,11 @@ func main() {
 	deviceController := controllers.NewDeviceController(session)
 
 	mux := mux.NewRouter()
-	mux.HandleFunc("/devices", deviceController.All()).Methods("GET")
-	mux.HandleFunc("/devices", deviceController.New()).Methods("POST")
-	mux.HandleFunc("/devices/{name}", deviceController.Update()).Methods("PUT")
-	mux.HandleFunc("/devices/{name}/{state}", deviceController.UpdateState()).Methods("PUT")
-	mux.HandleFunc("/devices/{port}", deviceController.DeviceByPort()).Methods("GET")
+	mux.HandleFunc("/api/devices", deviceController.All()).Methods("GET")
+	mux.HandleFunc("/api/devices", deviceController.New()).Methods("POST")
+	mux.HandleFunc("/api/devices/{name}", deviceController.Update()).Methods("PUT")
+	mux.HandleFunc("/api/devices/{name}/{state}", deviceController.UpdateState()).Methods("PUT")
+	mux.HandleFunc("/api/devices/{port}", deviceController.DeviceByPort()).Methods("GET")
 
 	http.ListenAndServe(":8080", mux)
 	log.Println("Server on port:8080")
